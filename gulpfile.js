68c20913ba6cd4377bb53f17c479e1a16447979e
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-13 17:03:03 +0200

    Parse our js code in src/ directory and translate JSX into JS (JSX => React Element)

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
+const gulp = require('gulp');
 const { watch } = require('gulp');
 const babel = require('gulp-babel');
 
+//Run each time a .js file change
 function js(cb) {
     console.log('js file change');
+    //Parse our js code in src/ directory and translate JSX into JS (JSX => React Element)
+    gulp.src('src/*.js')
+        .pipe(babel({
+            presets: ["@babel/preset-react"]
+        }))
+        //Place the output code in the dist/ directory
+        .pipe(gulp.dest('dist'))
     cb();
 }
 
 function defaultTask(cb) {
     console.log('do some work');
     //Gulp watch all events in .js files in the src/ directory and run js() task
-    watch('src/*.js', { events: 'all' }, js)
+    //ignoreInitial: false => run the js() task on initialization
+    watch('src/*.js', { ignoreInitial: false, events: 'all' }, js)
     cb();
 }
 
