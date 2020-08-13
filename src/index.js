d427407fcf5dc5e76abe7d8cf9a9256593f5a816
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-13 17:07:25 +0200

    Import React and ReactDOM thanks to skypack cdn provider and write our first react component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-console.log('User App');
+import * as React from 'https://cdn.skypack.dev/react@^16.13.1';
+import * as ReactDOM from 'https://cdn.skypack.dev/react-dom@^16.13.1';
 
 var x = 4;
+
+const App = () => {
+    return (
+        <div>
+            <h1>JSX Element</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem tempore velit, ipsam hic saepe provident, aperiam inventore illo quidem excepturi, consequuntur facere numquam ad harum temporibus aut quam recusandae? Dolorum.</p>
+        </div>
+    )
+}
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
