9069f7cec900d5a08d9bde25bffed45ead5535c5
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-13 17:21:16 +0200

    add a date element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import * as React from 'https://cdn.skypack.dev/react@^16.13.1';
 import * as ReactDOM from 'https://cdn.skypack.dev/react-dom@^16.13.1';
 
-var x = 4;
-
-const App = () => {
+//Our 
+const App = (props) => {
     return (
-        <div>
-            <h1>JSX Element</h1>
+        <React.Fragment>
+            <h1>My first component</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem tempore velit, ipsam hic saepe provident, aperiam inventore illo quidem excepturi, consequuntur facere numquam ad harum temporibus aut quam recusandae? Dolorum.</p>
-        </div>
+            <p>{props.date}</p>
+        </React.Fragment>
     )
 }
 
+//Get our root element DOM reference (as DOM Element() object)
 const root = document.getElementById("root");
 
-ReactDOM.render(<App />, root);
+const date = new Date();
+
+//Render our App component inside the root element of the document
+ReactDOM.render(<App date={date.toString()} />, root);
